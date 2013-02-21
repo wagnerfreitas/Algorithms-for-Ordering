8b651e89c93f2026c0250aa4476ea6453c4a0bc7
wagnersousa <wagner.sousa.f@gmail.com>
2013-02-21 18:00:10 -0300

    Criando BubbleSort.

---
+++ MetodosDeOrdenecao/MetodosDeOrdenacao.cs
@@ -0,0 +1,23 @@
+﻿namespace MetodosDeOrdenecao
+{
+    public class MetodosDeOrdenacao
+    {
+        public int[] BubbleSort(int[] vetor)
+        {
+            for (var i = 1; i < vetor.Length - 1; i++)
+            {
+                for (var j = 0; j < vetor.Length - 1 - i; j++)
+                {
+                    if (vetor[j] > vetor[j + 1])
+                    {
+                        var swap = vetor[j];
+                        vetor[j] = vetor[j + 1];
+                        vetor[j + 1] = swap;
+                    }
+                }
+            }
+
+            return vetor;
+        }
+    }
+}
