a4c22afec416ae5fea521718d68b1fe75c21371d
wagnersousa <wagner.sousa.f@gmail.com>
2013-02-28 13:02:42 -0300

    Criando BubbleSortWithFlag.

---
+++ MetodosDeOrdenecao.Testes/DadoMetodosDeOrdenacao.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace MetodosDeOrdenecao.Testes
+{
+    [TestFixture]
+    public class DadoMetodosDeOrdenacao
+    {
+        private int[] vetor;
+        private MetodosDeOrdenacao metodos;
+        private int[] vetorOrdenado;
+
+        [SetUp]
+        public void Cenario()
+        {
+            vetor = new[] { 1, 2, 7, 6, 7, 15, 3 };
+            metodos = new MetodosDeOrdenacao();
+            vetorOrdenado = new[] { 1, 2, 3, 6, 7, 7, 15 };
+        }
+
+        [Test]
+        public void PossoOrdenarUsandoBubbleSort()
+        {
+            metodos.BubbleSort(vetor);
+
+            Assert.True(SaoIguais(vetor, vetorOrdenado));
+        }
+
+        [Test]
+        public void PossoOrdenarUsandoBubbleSortWithFlag()
+        {
+            metodos.BubbleSortWithFlag(vetor);
+
+            Assert.That(SaoIguais(vetor, vetorOrdenado));
+        }
+
+        private bool SaoIguais(int[] vetor1, int[] vetor2)
+        {
+            var saoIguais = false;
+
+            for (var i = 0; i < vetor.Length; i++)
+            {
+                saoIguais = vetor1[i] == vetor2[i];
+            }
+
+            return saoIguais;
+        }
+    }
+}
--- MetodosDeOrdenecao/MetodosDeOrdenacao.cs
+++ MetodosDeOrdenecao/MetodosDeOrdenacao.cs
@@ -4,9 +4,9 @@
     {
         public int[] BubbleSort(int[] vetor)
         {
-            for (var i = 1; i < vetor.Length - 1; i++)
+            for (var i = 1; i < vetor.Length; i++)
             {
-                for (var j = 0; j < vetor.Length - 1 - i; j++)
+                for (var j = 0; j < vetor.Length - i; j++)
                 {
                     if (vetor[j] > vetor[j + 1])
                     {
@@ -16,6 +16,32 @@
                     }
                 }
             }
+
+            return vetor;
+        }
+
+        public int[] BubbleSortWithFlag(int[] vetor)
+        {
+            var i = 1;
+            bool flag;
+
+            do
+            {
+                flag = false;
+                for (var j = 0; j < vetor.Length - i; j++)
+                {
+                    if (vetor[j] > vetor[j + 1])
+                    {
+                        var swap = vetor[j];
+                        vetor[j] = vetor[j + 1];
+                        vetor[j + 1] = swap;
+
+                        flag = true;
+                    }
+                }
+
+                i++;
+            } while (flag);
 
             return vetor;
         }
